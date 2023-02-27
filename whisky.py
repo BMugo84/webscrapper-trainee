780eb385ad95d4cc9c85f06332594f419f3e091f
BMugo84 <mugo.benson.kbm@gmail.com>
2023-02-27 14:08:20 +0300

    can now check results of all pages

--- whisky.py
+++ whisky.py
@@ -3,6 +3,20 @@ from bs4 import BeautifulSoup
 
 baseurl = 'https://www.thewhiskyexchange.com/'
 
-headers = (
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
-)
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+}
+
+productLinks = []
+for x in range(1,4):
+    r = requests.get(f'https://www.thewhiskyexchange.com/c/35/japanese-whisky?pg={x}')
+
+    soup = BeautifulSoup(r.content, 'lxml')
+
+    productList = soup.find_all('li', attrs={'class':'product-grid__item'})
+
+    for item in productList:
+        for link in item.find_all('a', href=True):
+            productLinks.append(baseurl + link['href'])
+
+print(len(productLinks))
