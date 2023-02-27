f24e151c8fc8cc64b04c9ab69acdc13039049284
BMugo84 <mugo.benson.kbm@gmail.com>
2023-02-27 10:32:50 +0300

    learning webscrapping

---
+++ whisky.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+baseurl = 'https://www.thewhiskyexchange.com/'
+
+headers = (
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 OPR/95.0.0.0'
+)
