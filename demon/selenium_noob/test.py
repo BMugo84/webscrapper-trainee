db843cb7e000fe6c63626fcf21c06cc17eb666a0
BMugo84 <mugo.benson.kbm@gmail.com>
2023-03-02 10:01:32 +0300

    find elements

--- demon/selenium_noob/test.py
+++ demon/selenium_noob/test.py
@@ -1,3 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys #input keystrokes
 
 driver = webdriver.Firefox()
+
+url = 'https://the-internet.herokuapp.com/login'
+
+#   inspect element->copy ->XPath
+//*[@id="username"]
+//*[@id="password"]
+//*[@id="login"]/button
+
+#   tell the driver to find the elements
+driver.find_element_by_xpath('//*[@id="username"]')
+driver.find_element_by_xpath('//*[@id="password"]')
+driver.find_element_by_xpath('//*[@id="login"]/button')
+
+driver.get(url)
