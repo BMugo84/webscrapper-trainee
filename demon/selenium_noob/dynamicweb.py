3ba90bb771d92c8dd9aa4748ab3101523f6dccb1
BMugo84 <mugo.benson.kbm@gmail.com>
2023-03-02 17:41:35 +0300

    installed pandas and it works!

--- demon/selenium_noob/dynamicweb.py
+++ demon/selenium_noob/dynamicweb.py
@@ -1,6 +1,7 @@
 #   scraping data from youtube to get the top
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 url = 'https://www.youtube.com/@JohnWatsonRooney/videos'
 
@@ -17,9 +18,20 @@ driver.get(url)
 
 videos = driver.find_elements(By.CLASS_NAME, 'style-scope ytd-rich-grid-media')
 
+video_list = []
+
 for video in videos:
     title = video.find_element(By.XPATH, './/*[@id="video-title"]').text
     views = video.find_element(By.XPATH, './/*[@id="metadata-line"]/span[1]').text
     date  = video.find_element(By.XPATH, './/*[@id="metadata-line"]/span[2]').text
 
-    print(title, views, date)
+    video_item = {
+        'title':    title,
+        'views':    views,
+        'date':     date
+    }
+
+    video_list.append(video_item)
+
+    df = pd.DataFrame(video_list)
+    print(df)
