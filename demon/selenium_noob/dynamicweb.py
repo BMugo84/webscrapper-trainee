c187f03830235685f3507d842edd01a21aab01e6
BMugo84 <mugo.benson.kbm@gmail.com>
2023-03-02 17:31:56 +0300

    error: duplicate files received. changed using the dot

--- demon/selenium_noob/dynamicweb.py
+++ demon/selenium_noob/dynamicweb.py
@@ -18,8 +18,8 @@ driver.get(url)
 videos = driver.find_elements(By.CLASS_NAME, 'style-scope ytd-rich-grid-media')
 
 for video in videos:
-    title = video.find_element(By.XPATH, '//*[@id="video-title"]').text
-    views = video.find_element(By.XPATH, '//*[@id="metadata-line"]/span[1]').text
-    date  = video.find_element(By.XPATH, '//*[@id="metadata-line"]/span[2]').text
+    title = video.find_element(By.XPATH, './/*[@id="video-title"]').text
+    views = video.find_element(By.XPATH, './/*[@id="metadata-line"]/span[1]').text
+    date  = video.find_element(By.XPATH, './/*[@id="metadata-line"]/span[2]').text
 
     print(title, views, date)
