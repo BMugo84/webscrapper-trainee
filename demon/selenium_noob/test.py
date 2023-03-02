162fe3acbc5b527d4e6efde4d4b2ce3fed44cebb
BMugo84 <mugo.benson.kbm@gmail.com>
2023-03-02 10:20:50 +0300

    send keys to site

--- demon/selenium_noob/test.py
+++ demon/selenium_noob/test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys #input keystrokes
 
 driver = webdriver.Firefox()
@@ -6,13 +7,15 @@ driver = webdriver.Firefox()
 url = 'https://the-internet.herokuapp.com/login'
 
 #   inspect element->copy ->XPath
-//*[@id="username"]
-//*[@id="password"]
-//*[@id="login"]/button
-
-#   tell the driver to find the elements
-driver.find_element_by_xpath('//*[@id="username"]')
-driver.find_element_by_xpath('//*[@id="password"]')
-driver.find_element_by_xpath('//*[@id="login"]/button')
+# //*[@id="username"]
+# //*[@id="password"]
+# //*[@id="login"]/button
 
 driver.get(url)
+
+#   tell the driver to find the elements.
+#   send the keys to the site changed since 2020
+driver.find_element(By.XPATH, '//*[@id="username"]').send_keys('tomsmith')
+driver.find_element(By.XPATH, '//*[@id="password"]').send_keys('SuperSecretPassword!')
+driver.find_element(By.XPATH, '//*[@id="login"]/button').click()
+
