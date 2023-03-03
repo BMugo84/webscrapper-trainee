ce257060de5dc5041fe406b179d1c6554b81f19f
BMugo84 <mugo.benson.kbm@gmail.com>
2023-03-03 13:15:23 +0300

    removed popups and check whether i can recover phones

--- selenium_noob/ecom_scraper.py
+++ selenium_noob/ecom_scraper.py
@@ -1,10 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 
 url = 'https://www.jumia.co.ke/mobile-phones/?rating=3-5#catalog-listing'
-driver = webdriver.Firefox()
+#   remove popup ads
+options = Options()
+options.set_preference("dom.disable_beforeunload", True)
+options.set_preference("browser.tabs.warnOnClose", False)
+
+#   load driver
+driver = webdriver.Firefox(options=options)
 driver.get(url)
 
-//*[@id="close"]
-//*[@id="pop"]
+#   remove popup ads ie the newsletter. jumia detects whether you are a bot
+try:
+    close_button = driver.find_element(By.CSS_SELECTOR, 'button.cls[aria-label="newsletter_popup_close-cta"]')
+    close_button.click()
+except:
+    pass
+
+#   wait for content to load
+driver.implicitly_wait(30)
+
+#   get all phones
+phones = driver.find_elements(By.CSS_SELECTOR, 'article.prd')
+print(len(phones))
 
